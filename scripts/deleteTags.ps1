e9713cb19729ee16d4fc691754ccb12204be08b1
Senipah <n.morgan86@googlemail.com>
2019-11-05 12:04:56 +0000

    fix tag variable

--- scripts/deleteTags.ps1
+++ scripts/deleteTags.ps1
@@ -1,6 +1,6 @@
 $projectRoot = (Get-Item $PSScriptRoot).Parent
 Set-Location $projectRoot.FullName
 git tag | foreach-object -process { 
-    git push --delete origin tagName
+    git push --delete origin $_
     git tag -d $_ 
 }
